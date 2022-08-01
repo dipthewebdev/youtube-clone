6c115d2918ada209a587dc5106ae5f262e5ae398
Dipankar Das <dipankar19oct@gmail.com>
2022-08-01 23:12:05 +0530

    youtube clone

---
+++ script.js
@@ -0,0 +1,9 @@
+let menubtn = document.querySelector(".menu-btn");
+let sidebar = document.querySelector(".side-bar");
+
+let videoscontainer = document.querySelector(".videos-container");
+
+menubtn.onclick = function () {
+  sidebar.classList.toggle("resize-sidebar");
+  videoscontainer.classList.toggle("widen-videos-container");
+};
